3276afada7158f2f335b2ad8c2e7ceb56554fdfa
brankaDakic10 <branka.dakic@yahoo.com>
2018-04-24 18:47:50 +0200

    Finished 2.task

--- src/components/AnimalList.vue
+++ src/components/AnimalList.vue
@@ -8,11 +8,11 @@
     
   </thead>
   <tbody>
-  <tr v-for="(animal, key) in animals" :key="key">
+  <tr v-for="(animal, key) in animals" :key="key" >
 
     <td>{{animal.sort}}</td>
     <td>{{animal.name}}</td>
-   <td>{{animal.dateOfBirth}}</td>
+   <td>{{animal.dateOfBirth ||'nepoznat'}} </td>
     
     </tr>
    </tbody>
@@ -31,7 +31,7 @@ export default {
  animals:[
      {sort:'tiger',name:'Bill',dateOfBirth:'1.4.2017.'},
      {sort:'flamingo',name:'Lena',dateOfBirth:'13.10.2017.'},
-     {sort:'leon',name:'Sam',dateOfBirth:'13.1.2010.'},
+     {sort:'leon',name:'Sam',dateOfBirth:''},
      {sort:'monkey',name:'Joe',dateOfBirth:'11.11.2005.'},
      {sort:'elephant',name:'Nill',dateOfBirth:'19.1.2000.'}
    ]

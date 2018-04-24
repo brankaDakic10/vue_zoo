dc836eb13600c79354351b9fe2d620e30aad9ddf
brankaDakic10 <branka.dakic@yahoo.com>
2018-04-24 18:37:37 +0200

    Finished 1.task

---
+++ src/components/AnimalList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+   <table>
+  <thead>
+    <th>Sort</th>
+    <th>Name</th> 
+    <th>Date Of Birth</th>
+    
+  </thead>
+  <tbody>
+  <tr v-for="(animal, key) in animals" :key="key">
+
+    <td>{{animal.sort}}</td>
+    <td>{{animal.name}}</td>
+   <td>{{animal.dateOfBirth}}</td>
+    
+    </tr>
+   </tbody>
+</table>
+   
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'AnimalList',
+  data(){
+    return{
+
+ animals:[
+     {sort:'tiger',name:'Bill',dateOfBirth:'1.4.2017.'},
+     {sort:'flamingo',name:'Lena',dateOfBirth:'13.10.2017.'},
+     {sort:'leon',name:'Sam',dateOfBirth:'13.1.2010.'},
+     {sort:'monkey',name:'Joe',dateOfBirth:'11.11.2005.'},
+     {sort:'elephant',name:'Nill',dateOfBirth:'19.1.2000.'}
+   ]
+
+
+  }
+}
+}
+</script>
+
+</style>
